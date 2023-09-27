b692e66c15f5179165420783a0d6e0203bd82400
Mahmoud Ikhlaf <513095@student.fontys.nl>
2023-09-27 19:04:35 +0200

    Initial commit

---
+++ src/main/java/com/individueleproject/backendrunner/BackendRunnerApplication.java
@@ -0,0 +1,13 @@
+package com.individueleproject.backendrunner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendRunnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendRunnerApplication.class, args);
+	}
+
+}
